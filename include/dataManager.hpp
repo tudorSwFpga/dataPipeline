1aa8de1b2e5a57d653361e020797769117e47c9c
TudorSwFpga <tudor.constantinescu.fpga@gmail.com>
2024-09-23 17:56:30 +0200

    Clang format data manager + fix bugs shown by unit tests

--- include/dataManager.hpp
+++ include/dataManager.hpp
@@ -1,6 +1,6 @@
 #ifndef DATA_MANAGER_H
 #define DATA_MANAGER_H
-#include <plog/Log.h>
+#include "spdlog/spdlog.h"
 #include <queue>
 #include <array>
 #include <mutex>
@@ -15,100 +15,91 @@
    output queues.
 */
 
-enum MODE { BROADCAST, MAP};
-
+enum MODE { BROADCAST, MAP };
 
 template<class T>
-class DataManager: public Runnable
-{
-
+class DataManager {
 protected:
-	DataManager(const std::string& name):m_Name(name)
-	{
-		PLOG_DEBUG << "Constructor;";
-	}
+    DataManager(const std::string &name, uint8_t inQ, uint8_t outQ, MODE m) :
+        m_Name(name), m_maxNbInQueues(inQ), m_maxNbOutQueues(outQ), m_mode(m) {
+        spdlog::debug("DataManager Constructor;");
+    }
 
-	~DataManager(){
-		PLOG_DEBUG << " DataM Dtor";
+    ~DataManager() {
+        spdlog::debug(" DataM Destructor");
 
-		/*delete  m_inQueues;
-		delete  m_outQueues;
-		delete  m_inQueuesIds;
-		delete  m_outQueuesIds;*/
-	}
-
-	const  std::string m_Name;
-
+        /*delete  m_inQueues;
+        delete  m_outQueues;
+        delete  m_inQueuesIds;
+        delete  m_outQueuesIds;*/
+    }
+    const std::string m_Name;
 
 public:
+    // singleton class is not clonable
+    DataManager(DataManager &other) = delete;
+    // singleton class is not assignable
+    void operator=(const DataManager &) = delete;
+    // get access to the instance
 
-	//singleton class is not clonable
-	DataManager(DataManager &other) = delete;
-	//singleton class is not assignable
-	void operator=(const DataManager&) = delete;
-	//get access to the instance
+    static DataManager<T> *getInstance(const std::string &name);
 
-	static DataManager<T> *getInstance(const std::string& name);
+    // define number of input/output queues and the dispatching mode
+    bool setConf(const uint8_t &inQueues, const uint8_t &outQueues, const MODE &mode);
+    //
+    void reset();
+    // connect a feeder (get an id) to an input queue, if available
+    bool setFeeder(const std::string &appId);
+    // remove a feeder
+    bool remFeeder(const std::string &appId);
+    // push data to input queue of the feeder with pushId name
+    // connect a consumer (get an id) to an output queue, if available
+    bool setConsumer(const std::string &appId);
+    // remove a consumer
+    bool remConsumer(const std::string &appId);
+    //
+    void getConf(std::map<std::string, int> &inIds, std::map<std::string, int> &outIds);
+    void push(T &&data, const std::string &pushId);
+    // pop data from output queue of the consumer with popId nme
+    bool pop(const std::string &popId, std::vector<T> &data);
+    // get name
+    std::string getName();
 
+    std::condition_variable[] m_condVarOut;
 
-	//define number of input/output queues and the dispatching mode
-	bool setConf(const uint8_t& inQueues,const uint8_t& outQueues,const MODE& mode);
-	//connect a feeder (get an id) to an input queue, if available
-	bool  setFeeder(const std::string& appId);
-	//remove a feeder
-	bool  remFeeder(const std::string& appId);
-	//push data to input queue of the feeder with pushId name
-	//connect a consumer (get an id) to an output queue, if available
-	bool  setConsumer(const std::string& appId);
-	//remove a consumer
-	bool  remConsumer(const std::string& appId);
+    // TODO:
+    // setDecoder
+    // setParser
 
-	void push(T&& data, const std::string& pushId);
-	//pop data from output queue of the consumer with popId nme
-	bool pop(const std::string& popId, T* data);
-	
-	//TODO: 
-	//setDecoder
-	//setParser
-
-	
-	void  run();
-	void  stop();
-
+    void run();
+    void stop();
 
 private:
+    static DataManager<T> *m_Pinstance;
+    static std::mutex m_Mutex;
 
-	static DataManager<T> *m_Pinstance;
-	static std::mutex m_Mutex;
+    const uint8_t m_maxNbInQueues;
+    const uint8_t m_maxNbOutQueues;
+    const MODE m_mode;
 
-	uint8_t m_maxNbInQueues;
-	uint8_t m_maxNbOutQueues;
-	MODE m_mode;
+    bool m_isRunning;
+    std::vector<std::queue<T>> m_inQueues;
+    std::vector<std::queue<T>> m_outQueues;
 
-	bool m_isRunning;
-	std::vector<std::queue<T>> m_inQueues;
-	std::vector<std::queue<T>> m_outQueues;
+    std::map<std::string, int> m_inQueuesIds;
+    std::map<std::string, int> m_outQueuesIds;
+    // have here a routing table variable or some mode
 
-	std::map<std::string,int> m_inQueuesIds;
-	std::map<std::string,int> m_outQueuesIds;
-	//have here a routing table variable or some mode 
-
-	//mutexes for protecting acess to input / output queues
-	std::mutex m_inQueuesMutex;
-	std::condition_variable m_condVarIn;
-	std::mutex m_outQueuesMutex;
-	std::condition_variable m_condVarOut;
-	//broadcast all the input data to output queues
-	void manageBroadcast();
-	//route input packets to output queues based on a routing table
-	void manageMap();
-
-
+    // mutexes for protecting acess to input / output queues
+    std::mutex m_inQueuesMutex;
+    std::condition_variable m_condVarIn;
+    std::mutex m_outQueuesMutex;
+    // broadcast all the input data to output queues
+    void manageBroadcast();
+    // route input packets to output queues based on a routing table
+    void manageMap();
 };
-
 
 #include "../src/dataManager.cpp"
 
-
 #endif
-
--- src/dataManager.cpp
+++ src/dataManager.cpp
@@ -1,180 +1,209 @@
 #include <poll.h>
 #include <string>
-
-
-
-
-template<class T>
-DataManager<T>* DataManager<T>::getInstance(const std::string& name){
-	std::lock_guard<std::mutex> lock(m_Mutex);
-	if(m_Pinstance == nullptr){
-		m_Pinstance = new DataManager<T>(name);
-	}
-	return m_Pinstance;
-}
-
+#include <dataManager.hpp>
+#include "spdlog/spdlog.h"
 
 template<class T>
-bool DataManager<T>::setConf(const uint8_t& inQueues,const uint8_t& outQueues,const MODE& mode){
-	m_maxNbInQueues = inQueues;
-	m_maxNbOutQueues = outQueues;
-	m_mode = mode;
-	return true;
+DataManager<T> *DataManager<T>::getInstance(const std::string &name) {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+    if (m_Pinstance == nullptr) {
+        m_Pinstance = new DataManager<T>(name);
+    }
+    return m_Pinstance;
 }
 
 template<class T>
-void DataManager<T>::run(){
-	m_isRunning = true;
-	if (m_mode == BROADCAST) {
-		PLOG_DEBUG << " Manage data in broadcast mode";
-		DataManager::manageBroadcast();
-	} else {
-		DataManager::manageMap();
-	}
+bool DataManager<T>::setConf(const uint8_t &inQueues, const uint8_t &outQueues, const MODE &mode) {
+    m_maxNbInQueues  = inQueues;
+    m_maxNbOutQueues = outQueues;
+    m_mode           = mode;
+    return true;
 }
 
 template<class T>
-void DataManager<T>::stop(){
-	PLOG_DEBUG << " Stopping...";
-	m_isRunning = false;
-	m_condVarIn.notify_all();
-
+void DataManager<T>::getConf(std::map<std::string, int> &inIds, std::map<std::string, int> &outIds) {
+    inIds  = m_inQueuesIds;
+    outIds = m_outQueuesIds;
 }
 
+template<class T>
+void DataManager<T>::reset() {
+    m_inQueues.erase(m_inQueues.begin(), m_inQueues.end());
+    m_outQueues.erase(m_outQueues.begin(), m_outQueues.end());
 
+    m_inQueuesIds  = std::map<std::string, int>();
+    m_outQueuesIds = std::map<std::string, int>();
+    spdlog::debug("Size = ", m_outQueues.size());
+}
+
+template<class T>
+void DataManager<T>::run() {
+    m_isRunning = true;
+    do {
+        if (m_mode == BROADCAST) {
+            DataManager::manageBroadcast();
+        } else {
+            DataManager::manageMap();
+        }
+    } while (m_isRunning);
+    spdlog::debug(" DataManager stopped");
+}
+
+template<class T>
+void DataManager<T>::stop() {
+    spdlog::debug(" DataManager stopping...");
+    m_isRunning = false;
+    for (auto cv : m_condVarIn) {
+        cv.notify_all();
+    }
+    for (auto &cv : m_condVarOut) {
+        cv.notify_all();
+    }
+}
 /*
 check each input queue status and copy on every output queue
 */
 template<class T>
-void DataManager<T>::manageBroadcast(){
-	PLOG_DEBUG << " Waiting...";
-	std::unique_lock<std::mutex> lk(m_inQueuesMutex);
-	m_condVarIn.wait(lk, [this] {
-		return m_isRunning == false;
-	} );
-	T data;
-	for (auto &it:m_inQueues){
-		if (!it.empty()){	
-			data = it.front();
-			it.pop();
-			{	//local context to reduce lock time acquisition
-				PLOG_DEBUG << " Acquire out lock";
-				std::unique_lock<std::mutex> lockout(m_outQueuesMutex);
-				for (auto &it:m_outQueues) {
-					it.push(data);
-					PLOG_DEBUG << " Pushing new data " << data << " to output q;Notyifing consumers!";
-				}
-				m_condVarOut.notify_all();
-			}	
-		}
-	}
+void DataManager<T>::manageBroadcast() {
+    spdlog::debug("DataManager::manageBroadcast Waiting...");
+    std::unique_lock<std::mutex> lk(m_inQueuesMutex);
+    m_condVarIn.wait(lk);
+    T data;
+    int i = 0;
+    for (auto &it : m_inQueues) {
+        spdlog::debug("manageBroadcast wtf is it happening");
+        if (!it.empty()) {
+            data = it.front();
+            it.pop();
+            { // local context to reduce lock time acquisition
+                spdlog::debug(" Acquire out lock");
+                std::unique_lock<std::mutex> lockout(m_outQueuesMutex);
+                for (auto &it : m_outQueues) {
+                    it.push(data);
+                    spdlog::debug(" Pushing new data {} to output q;Notyifing consumers!", data);
+                }
+                m_condVarOut[i].notify_all();
+                // TOOD: replace here by something that also notifies users
+            }
+        }
+        i++;
+    }
 }
 
 template<class T>
-void DataManager<T>::manageMap(){
-}
-
+void DataManager<T>::manageMap() {}
 
 template<class T>
-void DataManager<T>::push(T&& data,const std::string& pushId){
-	//acquire mutex and push data into the input queue 
-	std::unique_lock<std::mutex> guard(m_inQueuesMutex);
-	if  (m_inQueuesIds.count(pushId) == 0) {
-		PLOG_ERROR << "No input queue with ID: " << pushId;
-	} else {
-		m_inQueues[m_inQueuesIds[pushId]].push(data);
-		PLOG_INFO << " Pushed new data " << data << " in queue " <<  std::to_string(m_inQueuesIds[pushId]);
-		//notify thread waiting waiting for input data
-		m_condVarIn.notify_one();
-	}
-}
-
-
-template<class T>
-bool DataManager<T>::pop(const std::string& popId, T* data){
-	//acquire mutex and push data into the input queue 
-	if  (m_outQueuesIds.count(popId) == 0) {
-		PLOG_ERROR << "No output queue with ID: " << popId;
-		return false;
-	} else {
-		std::unique_lock<std::mutex> lk(m_outQueuesMutex);
-		PLOG_DEBUG << "Waiting until corresponding output queue " << m_outQueuesIds[popId] <<  " is filled...";
-		//auto t = m_outQueues[m_outQueuesIds[popId]].empty();
-		//while (m_outQueues[m_outQueuesIds[popId]].empty()){
-				m_condVarOut.wait(lk);
-		//}
-		//pop oldest data and then erase it
-		data[0] = m_outQueues[m_outQueuesIds[popId]].front();
-		m_outQueues[m_outQueuesIds[popId]].pop();
-		PLOG_INFO << "Returned data from output queue " << popId << " " << data[0];
-		return true;	
-	}
+void DataManager<T>::push(T &&data, const std::string &pushId) {
+    // acquire mutex and push data into the input queue
+    std::unique_lock<std::mutex> guard(m_inQueuesMutex);
+    if (m_inQueuesIds.count(pushId) == 0) {
+        spdlog::error("No input queue with ID: {}", pushId);
+    } else {
+        m_inQueues[m_inQueuesIds[pushId]].push(data);
+        spdlog::info(" Pushed new data {} in queue {}", data, std::to_string(m_inQueuesIds[pushId]));
+        // notify thread waiting waiting for input data
+        m_condVarIn.notify_one();
+    }
 }
 
 template<class T>
-bool  DataManager<T>::setFeeder(const std::string& appId){
-	if (m_inQueuesIds.count(appId) == 0){
-		//initially all the map is empty 
-		if (m_inQueuesIds.size() < m_maxNbInQueues){
-			m_inQueuesIds.emplace(std::make_pair(appId,m_inQueuesIds.size()));
-			m_inQueues.push_back(std::queue<T>());
-			PLOG_DEBUG << "New Feeder: " << appId;
-
-		//but once it becomes full, we can only find place if a feeder has left
-		} else {
-			auto f = m_inQueuesIds.find("empty");
-			if (f != m_inQueuesIds.end()){
-				m_inQueuesIds.emplace(std::make_pair(appId,f->second));
-				m_inQueuesIds.erase(f);
-				PLOG_DEBUG << "New Feeder: " << appId;
-				return true;
-			} else {
-				PLOG_ERROR << " No empty place to set new Feeder " << appId;	
-				return false;
-			}
-		}
-	} else {
-		PLOG_ERROR << " Feeder already existing:  " << appId;;	
-		return false;
-	}
+bool DataManager<T>::pop(const std::string &popId, std::vector<T> &data) {
+    // acquire mutex and push data into the input queue
+    if (m_outQueuesIds.count(popId) == 0) {
+        spdlog::error("No output queue with ID: " + popId);
+        return false;
+    } else {
+        std::unique_lock<std::mutex> lk(m_outQueuesMutex);
+        const int id = m_outQueuesIds[popId];
+        m_condVarOut[id].wait(lk);
+        // pop oldest data and then erase it
+        while (!m_outQueues[m_outQueuesIds[popId]].empty()) {
+            data.push_back(m_outQueues[m_outQueuesIds[popId]].front());
+            m_outQueues[m_outQueuesIds[popId]].pop();
+        }
+        spdlog::debug("Popping data from output queue {}", popId);
+        return true;
+    }
 }
-
 
 template<class T>
-bool  DataManager<T>::setConsumer(const std::string& appId){
-	if (m_outQueuesIds.count(appId) == 0){
-		//initially all the map is empty
-		if (m_outQueuesIds.size() < m_maxNbOutQueues){
-			m_outQueuesIds.emplace(std::make_pair(appId,m_outQueuesIds.size()));
-			m_outQueues.push_back(std::queue<T>());
-			PLOG_INFO << "New Consumer " << appId;	
+bool DataManager<T>::setFeeder(const std::string &appId) {
+    if (m_inQueuesIds.count(appId) == 0) {
+        // initially all the map is empty
+        if (m_inQueuesIds.size() < m_maxNbInQueues) {
+            m_inQueuesIds.emplace(std::make_pair(appId, m_inQueuesIds.size()));
+            m_inQueues.push_back(std::queue<T>());
+            spdlog::debug("New Feeder: {}", appId);
 
-		//but once it becomes full, we can only find place if a feeder has left
-		} else {
-			auto f = m_outQueuesIds.find("empty");
-			if (f != m_outQueuesIds.end()){
-				m_outQueuesIds.emplace(std::make_pair(appId,f->second));
-				m_outQueuesIds.erase(f);
-				PLOG_INFO << "New Consumer: " << appId;
-				return true;
-			} else {
-				PLOG_ERROR << " No empty place to set new Consumer " << appId;	
-				return false;
-			}
-		}
-	} else {
-		PLOG_ERROR << " Consumer already existing:  " << appId;;	
-		return false;
-	}
+            // but once it becomes full, we can only find place if a feeder has left
+        } else {
+            auto f = m_inQueuesIds.find("empty");
+            if (f != m_inQueuesIds.end()) {
+                m_inQueuesIds.emplace(std::make_pair(appId, f->second));
+                m_inQueuesIds.erase(f);
+                spdlog::debug("New Feeder: {}", appId);
+                return true;
+            } else {
+                spdlog::error(" No empty place to set new Feeder " + appId);
+                return false;
+            }
+        }
+    } else {
+        spdlog::error(" Feeder already existing:  " + appId);
+        return false;
+    }
 }
 
+template<class T>
+bool DataManager<T>::remFeeder(const std::string &appId) {
+    if (m_inQueuesIds.count(appId) == 0) {
+        return false;
+    } else {
+        auto f = m_inQueuesIds.find(appId);
+        m_inQueuesIds.erase(f);
+        return true;
+    }
+}
 
+template<class T>
+bool DataManager<T>::setConsumer(const std::string &appId) {
+    if (m_outQueuesIds.count(appId) == 0) {
+        // initially all the map is empty
+        if (m_outQueuesIds.size() < m_maxNbOutQueues) {
+            m_outQueuesIds.emplace(std::make_pair(appId, m_outQueuesIds.size()));
+            // m_hasData.emplace(std::make_pair(appId,false));
+            m_outQueues.push_back(std::queue<T>());
+            spdlog::info("New Consumer " + appId);
+            spdlog::info("New number of out queues {}", m_outQueues.size());
+            // but once it becomes full, we can only find place if a feeder has left
+        } else {
+            auto f = m_outQueuesIds.find("empty");
+            if (f != m_outQueuesIds.end()) {
+                m_outQueuesIds.emplace(std::make_pair(appId, f->second));
+                m_outQueuesIds.erase(f);
+                spdlog::info("New Consumer 2 " + appId);
+                return true;
+            } else {
+                spdlog::error("No empty place to set new Consumer " + appId);
+                return false;
+            }
+        }
+    } else {
+        spdlog::error("Consumer already existing: " + appId);
+        return false;
+    }
+}
 
-template class  DataManager<std::string>;
-template <class T>
-DataManager<T>* DataManager<T>::m_Pinstance{nullptr};
-template <class T>
+template<class T>
+std::string DataManager<T>::getName() {
+    return m_Name;
+}
+
+template class DataManager<std::string>;
+template<class T>
+DataManager<T> *DataManager<T>::m_Pinstance{nullptr};
+template<class T>
 std::mutex DataManager<T>::m_Mutex;
 
-//template class  DataManager<int>;
-
+// template class  DataManager<int>;
