008396fbf9b3dcd983d7720bfb0c05dcf40aad88
TudorSwFpga <tudor.constantinescu.fpga@gmail.com>
2024-10-17 18:55:36 +0200

    Changed queue to dequeue and added a ton of other methods so than we can copy or move data between 2 thread safe queues

--- include/queue.hpp
+++ include/queue.hpp
@@ -1,19 +1,60 @@
 #pragma once
 #include <mutex>
-#include <queue>
+#include <deque>
+#include <vector>
 #include <condition_variable>
+#include <list>
+#include <iostream>
 
 template<class T>
 class Q {
 public:
-    Q(){};
-    ~Q(){};
-    void push(const T &);
-    void pop(T &data);
-    void stop();
+    Q() {
+        std::cout << "Q ctor" << std::endl;
+    };
 
-private:
-    std::queue<T> queue;        // Dedicated queue for each consumer
+    // Delete the copy constructor and copy assignment operator
+    Q(const Q &)            = delete;
+    Q &operator=(const Q &) = delete;
+
+    // Move constructor
+    Q(Q &&other) noexcept {
+        std::lock_guard<std::mutex> lock(other.mtx); // Ensure thread-safety during move
+        queue = std::move(other.queue);
+    }
+
+    // Define move assignment operator
+    Q &operator=(Q &&other) noexcept {
+        if (this != &other) {
+            std::lock_guard<std::mutex> lock(other.mtx); // Ensure thread-safety during move
+            queue = std::move(other.queue);
+        }
+        return *this;
+    }
+
+    ~Q() {
+        std::cout << "Q dtor" << std::endl;
+    };
+
+    // copy data to another Q
+    void copy(Q<T> &other);
+    // copy data to several Q, enable signal saying to which
+    void copy(std::vector<Q<T>> &others, std::list<int> enableCopy);
+    // move data to another Q
+    void move(std::vector<T> &other);
+    // push data into Q
+    void push(const T &data);
+    void push(const T &&data);
+
+    void pop(T &data, bool blocking);
+    void pop(bool blocking);
+    void stop();
+    void pause();
+    void restart();
+    T front();
+    bool empty();
+
+    std::deque<T> queue;
     std::mutex mtx;             // Mutex to protect the queue
     std::condition_variable cv; // Condition variable to notify the consumer
 };
--- src/queue.cpp
+++ src/queue.cpp
@@ -1,24 +1,93 @@
 #include <iostream>
+#include <algorithm>
+#include <utility>
 
 template<class T>
 void Q<T>::push(const T &data) {
     std::lock_guard<std::mutex> lock(mtx);
-    queue.push(data);
+    queue.push_back(data);
     cv.notify_all();
 }
 
 template<class T>
-void Q<T>::pop(T &data) {
-    std::unique_lock<std::mutex> lock(mtx);
-    cv.wait(lock);
-    data = queue.front();
-    queue.pop();
+void Q<T>::push(const T &&data) {
+    std::lock_guard<std::mutex> lock(mtx);
+    queue.push_back(data);
     cv.notify_all();
+}
+
+template<class T>
+void Q<T>::pop(T &data, bool blocking) {
+    std::unique_lock<std::mutex> lock(mtx);
+    if (blocking)
+        cv.wait(lock);
+    data = queue.front();
+    queue.pop_front();
+    cv.notify_all();
+}
+
+template<class T>
+void Q<T>::pop(bool blocking) {
+    std::unique_lock<std::mutex> lock(mtx);
+    if (blocking)
+        cv.wait(lock);
+    queue.pop_front();
+    cv.notify_all();
+}
+
+template<class T>
+void Q<T>::copy(Q<T> &other) {
+    std::unique_lock<std::mutex> lock_src(mtx);
+    std::unique_lock<std::mutex> lock_dst(other.mtx);
+    std::copy(queue.begin(), queue.end(), std::back_inserter(other.queue));
+    other.cv.notify_all();
+}
+
+// copy one queue to several queues, with a list saying which ones have to be copied
+template<class T>
+void Q<T>::copy(std::vector<Q<T>> &others, std::list<int> enableCopy) {
+    std::unique_lock<std::mutex> lock_src(mtx);
+    for (auto id : enableCopy) {
+        std::unique_lock<std::mutex> lock_dst(others[id].mtx);
+        std::copy(queue.begin(), queue.end(), std::back_inserter(others[id].queue));
+        others[id].cv.notify_all();
+    }
+    queue.clear();
+}
+
+template<class T>
+void Q<T>::move(std::vector<T> &other) {
+    std::unique_lock<std::mutex> lock_src(mtx);
+    // TODO: moving works with strings but will not work with some types such as integers
+    other.insert(other.end(), std::make_move_iterator(queue.begin()), std::make_move_iterator(queue.end()));
+    queue.clear();
 }
 
 template<class T>
 void Q<T>::stop() {
     cv.notify_all();
+}
+
+template<class T>
+bool Q<T>::empty() {
+    std::unique_lock<std::mutex> lock(mtx);
+    return queue.empty();
+}
+
+template<class T>
+T Q<T>::front() {
+    return queue.front();
+}
+
+template<class T>
+void Q<T>::pause() {
+    mtx.lock();
+}
+
+template<class T>
+void Q<T>::restart() {
+    queue.clear();
+    mtx.unlock();
 }
 
 template class Q<std::string>;
