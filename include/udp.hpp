e00e7e9d023cd5214b222c3fa135ddd668e8751c
Tudor Constantinescu <tudor.constantinescu.fpga@gmail.com>
2025-02-19 21:49:45 +0100

    Set udp socket to unblocking and other fixes (remove m_handler which is contained by the proxy node)

--- include/udp.hpp
+++ include/udp.hpp
@@ -6,10 +6,10 @@
 
 class UdpServer : public ProxyNode {
 public:
-    UdpServer(const std::string &name, const uint16_t &udpPort, DataManager<std::string> *dataHandler) :
-        ProxyNode(name, udpPort, dataHandler), m_name(name) {
-        this->m_dataHandler->setFeeder(name);
+    UdpServer(const std::string &name, const uint16_t &tcpPort, DataManager<std::string> *dataHandler) :
+        ProxyNode(name, tcpPort, dataHandler) {
         spdlog::debug("UDP Server ctor ");
+        m_dataHandler->setFeeder(name);
         m_sockfd = createRxSocket();
     }
     ~UdpServer() {
@@ -17,31 +17,32 @@ public:
     }
     // in running state all the magic operates
     bool run() override;
+    std::string getName();
 
 private:
-    const std::string m_name;
     int m_sockfd;
     struct sockaddr_in m_Address;
     // data handler, used when receiving packets
-    std::shared_ptr<DataManager<std::string>> m_dataHandler;
     int createRxSocket();
 };
 
 class UdpClient : public ProxyNode {
 public:
-    UdpClient(const std::string &name, const uint16_t& rem_port, DataManager<std::string> *dataHandler) : 
+    UdpClient(const std::string &name, const uint16_t &rem_port, DataManager<std::string> *dataHandler) :
         ProxyNode(name, rem_port, dataHandler) {
         spdlog::debug("UDP Client ctor ");
         createTxSocket();
-        //TODO: Better manage the return value
+        // TODO: Better manage the return value
     }
     ~UdpClient() {
         spdlog::debug("UDP Client dtor ");
     }
+
+    int send(const std::string &msg);
+
 private:
     int m_sockfd;
     struct sockaddr_in m_address;
-    int send(const std::string &msg);
     void createTxSocket();
     bool run() override;
 };
--- src/udp.cpp
+++ src/udp.cpp
@@ -1,6 +1,7 @@
 #include "../include/udp.hpp"
 #include <stdexcept>
 #include <errno.h>
+#include <fcntl.h>
 
 int UdpServer::createRxSocket() {
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -18,6 +19,13 @@ int UdpServer::createRxSocket() {
     if (bind(sockfd, (struct sockaddr *)&m_Address, sizeof(sockaddr_in)) < 0) {
         throw std::runtime_error(std::string("Cannot bind to asked port ") + std::string(std::strerror(errno)));
     }
+
+    // set socket to non-blocking
+    int flags = fcntl(sockfd, F_GETFL, 0);
+    flags |= O_NONBLOCK;
+    if (fcntl(sockfd, F_SETFL, flags) == -1) {
+        spdlog::error("Error setting file descriptor to non-blocking");
+    }
     return sockfd;
 }
 
@@ -27,13 +35,17 @@ bool UdpServer::run() {
     socklen_t addr_len = sizeof(m_Address);
     int n              = recvfrom(m_sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&m_Address, &addr_len);
     if (n < 0) {
-        spdlog::error("Error receiving data");
+        spdlog::debug("No received data");
     } else {
         spdlog::debug("Received data: {}", buffer);
-        //TODO push should return a bool that should be checked
+        // TODO push should return a bool that should be checked
         m_dataHandler->push(std::string(buffer), m_name);
     }
     return true;
+}
+
+std::string UdpServer::getName() {
+    return m_name;
 }
 
 void UdpClient::createTxSocket() {
@@ -51,7 +63,7 @@ void UdpClient::createTxSocket() {
     m_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 }
 
-bool UdpClient::run(){
+bool UdpClient::run() {
     std::vector<std::string> data;
     bool ret = true;
     if (m_dataHandler->pop(m_name, data)) {
@@ -60,9 +72,13 @@ bool UdpClient::run(){
             if (::sendto(m_sockfd, it.c_str(), it.size(), 0, (struct sockaddr *)&m_address, sizeof(sockaddr_in)) < 0) {
                 spdlog::error("Error sending data");
                 ret = false;
-            } 
+            }
         }
         data.clear();
     }
     return ret;
+}
+
+int UdpClient::send(const std::string &msg) {
+    return ::sendto(m_sockfd, msg.c_str(), msg.size(), 0, (struct sockaddr *)&m_address, sizeof(sockaddr_in));
 }
